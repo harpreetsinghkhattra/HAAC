e16e5d476ca974300f69e677210cdfee6832f63d
harpreetsinghkhattra <harpreetsinghkhattra@gmail.com>
2016-10-12 19:35:58 -0700

    commit message by hac

---
+++ app/src/main/java/HAC_front_page.java
@@ -0,0 +1,17 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.happy.hac.R;
+
+/**
+ * Created by Happy on 10/12/2016.
+ */
+
+public class HAC_front_page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hac_front_page);
+    }
+}
